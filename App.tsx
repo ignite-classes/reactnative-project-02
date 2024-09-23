473adfea85787e01e412b60b333b97c47ed6abb2
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2024-09-23 16:10:39 -0300

    Resolvendo conflito de tipagem do Styled Component - 2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components'
 import { Groups } from "@screens/Groups";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
 
 import theme from './src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-  <ThemeProvider theme={theme}>
-    <Groups />
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading /> }
+    </ThemeProvider>
   )
 }
